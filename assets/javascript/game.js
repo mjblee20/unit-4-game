6fe9a44d4cf8bc6379eb574aed2ed450329a8423
Monte Cristo <mjblee20@Bills-MacBook-Pro.local>
2019-03-06 21:39:11 -0800

    functioning chooseChar

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,54 +17,64 @@ var enemy = {
 var attackCount = 1; 
 // player is player = 0, enemy is player = 1
 var player = 0;
+var chosenEnemy = [];
+var chosenPlayer;
+var inGame = false;
+var enemyNumber;
 
 $("document").ready(function() {
+    console.log(document.getElementById("playerChar"));
     $("#attack").on("click", function() {
         attack();
     });
-    $("img").on("click", function() {
+    $("#restart").on("click", function() {
+        restart();
+    });
+    $(".character").on("click", function() {
         console.log(this.id);
         if (player === 0) {
             chooseChar(this);
+            player.healthPoints = parseInt(this.dataset["health"]);
+            player.attackPower = parseInt(this.dataset["attack"]);
+            player.counterAttackPower= parseInt(this.dataset["counter"]);
+            console.log(this.dataset["health"], this.dataset["attack"], this.dataset["counter"]);
+            chosenPlayer = this;
         } else {
             chooseEnemy(this); 
+            enemy.healthPoints = parseInt(this.dataset["health"]);
+            enemy.attackPower = parseInt(this.dataset["attack"]);
+            enemy.counterAttackPower= parseInt(this.dataset["counter"]);
+            // keep track of the hidden defeated character cards
+            chosenEnemy.push(this);
         }  
             
     });
-    gameStart();
 });
-
-// initialize game 
-function gameStart() {
-    //allow 
-    
-}
 
 // player chooses a character by selecting character's picture
 function chooseChar(obj) {
+    console.log("in chooseChar");
     // player character box moves to the playerCharacter side of the battlefield
-    $("#"+obj.id).parent().appendTo($("#playerChar"));
-    
+    $("#"+obj.id).appendTo($("#playerChar"));
+
     
     // move all other characters to the Enemies Available To Attack
-    console.log($("#start")[0].childNodes);
-    var children = $("#start")[0].childNodes
-    for (let i = 0; i < children.length; i++) {
+    var children;
 
-    }
 
     player = 1;
 }
 
 // player chooses opponent by selecting character's picture, defender is moved to defender_area, button attack is now enabled
 function chooseEnemy(obj) {
-    $("#"+this.id).parent().appendTo($("#enemyChar"));
+    console.log("in chooseEnemy");
+    enemyNumber = 1;
+    inGame = true;
+    // $("#"+obj.id).parent().appendTo($("#enemyChar"));
     console.log("choosing Enemy Character")
     // move the selected enemy to enemyChar side of the battlefield
-    $("img").on("click", function() {
-        $("#"+obj.id).parent().appendTo($("#enemyChar"));
-    });
-    // 
+    // enables the attack button
+    document.getElementById("attack").disabled = false;
 
 }
 
@@ -79,17 +89,42 @@ function attack() {
     // each time the player attacks, their character's Attack Power increases by its base Attack Power.
     attackCount++;
     // get attack value from html and update it
-
+    console.log(document.getElementById("playerChar"));
+    player.attackPower *= attackCount;
+    chosenPlayer.data["attackPower"] = "" + player.attackPower
     // if player char health is <= 0, player has lost display You have been defeated... GG! in log
+    if (player.healthPoint <= 0) {
+        inGame = false;
+        // show restart button
+        document.getElementById("restart").style.display = "block";
+        // disable attack button
+        document.getElementById("attack").disabled = true;
+        // defeat message
+        document.getElementById("log").innerHTML = "You have been defeated... GG!";
+    }
 
-
-    // if enemy char health is <= 0, remove the character box from enemyChar in battlefield
+    // if enemy char health is <= 0, remove the character box from enemyChar in battlefield by changing display to none in css stylesheet
     // initiate another chooseEnemy, if there are no more enemies initiate win screen
-    if (#) {
+    if (enemy.healthPoints <= 0) {
+        inGame = false;
+        // show restart button
+        document.getElementById("restart").style.display = "block";
+        chosenEnemy.style.display = "none";
+        // checks if inGame is true and if all enemies have been defeated
+        if (enemyNumber === chosenEnemy.length) {
+            document.getElementById("log").innerHTML = "You have been victorious!... GG!";
+        }
 
     }
 }
 
 function restart() {
-
+    // show all the character boxes
+    characters = $(".character");
+    for (let i = 0; i < character.length; i++) {
+        characters[i].style.display = "block";
+    }
+    // reset inGame
+    
+    document.getElementById("restart").style.display = "none";
 }
